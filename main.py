9df9a15554e192276710bacff0c3829d149833f4
MikeR <mike.rogers@image-metrics.com>
2024-09-28 14:08:17 -0400

    try to run

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from datetime import datetime
 import time
-import sys
 
 from nyct_gtfs import NYCTFeed
-# if sys.platform == 'win32':
-#     from RGBMatrixEmulator import graphics
-# else:
-#     from rgbmatrix import graphics
-#
-# from samplebase import SampleBase
+# from RGBMatrixEmulator import graphics
+from rgbmatrix import graphics
+
+from samplebase import SampleBase
 
 FEEDS = [
     NYCTFeed("F"),
@@ -18,132 +15,132 @@ FEEDS = [
 NOW = datetime.now()
 
 
-# class DisplayTrains(SampleBase):
-#     def __init__(self, stop_ids, *args, **kwargs):
-#         super(DisplayTrains, self).__init__(*args, **kwargs)
-#
-#         self.stop_ids = stop_ids
-#         self.font = graphics.Font()
-#         # self.font.LoadFont("./fonts/7x13.bdf")
-#         self.font.LoadFont("./fonts/helvR12.bdf")
-#         self.circle_font = graphics.Font()
-#         self.circle_font.LoadFont('./fonts/6x10.bdf')
-#
-#     def draw_filled_circle(self, canvas, x, y, color):
-#         # # Draw circle with lines
-#         # graphics.DrawLine(canvas, x - 2, y - 6, x + 2, y - 6, color)
-#         # graphics.DrawLine(canvas, x - 3, y - 5, x + 3, y - 5, color)
-#         # graphics.DrawLine(canvas, x - 4, y - 4, x + 4, y - 4, color)
-#         # graphics.DrawLine(canvas, x - 5, y - 3, x + 5, y - 3, color)
-#         # graphics.DrawLine(canvas, x - 6, y - 2, x + 6, y - 2, color)
-#         # graphics.DrawLine(canvas, x - 6, y - 1, x + 6, y - 1, color)
-#         # graphics.DrawLine(canvas, x - 6, y, x + 6, y, color)
-#         # graphics.DrawLine(canvas, x - 6, y + 1, x + 6, y + 1, color)
-#         # graphics.DrawLine(canvas, x - 6, y + 2, x + 6, y + 2, color)
-#         # graphics.DrawLine(canvas, x - 5, y + 3, x + 5, y + 3, color)
-#         # graphics.DrawLine(canvas, x - 4, y + 4, x + 4, y + 4, color)
-#         # graphics.DrawLine(canvas, x - 3, y + 5, x + 3, y + 5, color)
-#         # graphics.DrawLine(canvas, x - 2, y + 6, x + 2, y + 6, color)
-#
-#         # Draw circle with lines
-#         graphics.DrawLine(canvas, x - 2, y - 5, x + 2, y - 5, color)
-#         graphics.DrawLine(canvas, x - 3, y - 4, x + 3, y - 4, color)
-#         graphics.DrawLine(canvas, x - 4, y - 3, x + 4, y - 3, color)
-#         graphics.DrawLine(canvas, x - 5, y - 2, x + 5, y - 2, color)
-#         graphics.DrawLine(canvas, x - 5, y - 1, x + 5, y - 1, color)
-#         graphics.DrawLine(canvas, x - 5, y, x + 5, y, color)
-#         graphics.DrawLine(canvas, x - 5, y + 1, x + 5, y + 1, color)
-#         graphics.DrawLine(canvas, x - 5, y + 2, x + 5, y + 2, color)
-#         graphics.DrawLine(canvas, x - 4, y + 3, x + 4, y + 3, color)
-#         graphics.DrawLine(canvas, x - 3, y + 4, x + 3, y + 4, color)
-#         graphics.DrawLine(canvas, x - 2, y + 5, x + 2, y + 5, color)
-#
-#     def draw_row(self,
-#                  canvas,
-#                  row_ind,
-#                  text_colour,
-#                  circle_colour,
-#                  route_id,
-#                  headsign_text,
-#                  direction,
-#                  arrival_mins):
-#         # Top line
-#         if row_ind == 0:
-#             circle_y = 7
-#             text_y = 12
-#         else:
-#             # Bottom line
-#             circle_y = 23
-#             text_y = 28
-#
-#         route_id_offset_width = self.circle_font.CharacterWidth(ord(route_id))
-#         route_id_offset = int(route_id_offset_width / 2) - 1
-#
-#         minutes_text = f'{arrival_mins:2d}'
-#         minutes_width = sum(self.font.CharacterWidth(ord(letter)) for letter in minutes_text)
-#
-#         graphics.DrawText(canvas, self.font, 0, text_y, text_colour, f'{row_ind+1}')
-#         graphics.DrawText(canvas, self.font, 6, text_y, text_colour, f'.')
-#         # graphics.DrawCircle(canvas, 16, circle_y, 5, circle_colour)
-#         self.draw_filled_circle(canvas, 16, circle_y, circle_colour)
-#         graphics.DrawText(canvas, self.circle_font, 16 - route_id_offset, text_y-1, graphics.Color(0, 0, 0), route_id)
-#         # graphics.DrawText(canvas, self.font, 26, text_y, text_colour, headsign_text)
-#         if direction == 'N':
-#             graphics.DrawText(canvas, self.circle_font, 23, text_y - 1, text_colour, '↑')
-#         else:
-#             graphics.DrawText(canvas, self.circle_font, 23, text_y - 1, text_colour, '↓')
-#         graphics.DrawText(canvas, self.font, 43 - minutes_width, text_y, text_colour, minutes_text)
-#         graphics.DrawText(canvas, self.font, 43, text_y, text_colour, "min")
-#
-#     def draw_train(self, row_ind, train, stop_id, canvas):
-#         arrival_mins = arrival_minutes(train, stop_id)
-#         text_color = graphics.Color(0, 110, 0)
-#         if arrival_mins <= 0:
-#             text_color = graphics.Color(255, 66, 25)
-#
-#         # see: https://www.6sqft.com/did-you-know-the-mta-uses-pantone-colors-to-distinguish-train-lines/
-#         if arrival_mins <= 0:
-#             circle_color = graphics.Color(255, 66, 25)
-#         elif train.route_id in ['B', 'D', 'F', 'M']:
-#             circle_color = graphics.Color(255, 66, 25)
-#         elif train.route_id in ['G', ]:
-#             circle_color = graphics.Color(108, 190, 69)
-#         else:
-#             circle_color = graphics.Color(252, 204, 10)
-#
-#         if stop_id.endswith('N'):
-#             direction = 'N'
-#         else:
-#             direction = 'S'
-#
-#         self.draw_row(canvas,
-#                       row_ind=row_ind,
-#                       text_colour=text_color,
-#                       circle_colour=circle_color,
-#                       route_id=train.route_id,
-#                       headsign_text=train.headsign_text,
-#                       direction=direction,
-#                       arrival_mins=arrival_mins)
-#
-#     def draw_trains(self, trains, stop_id):
-#         canvas = self.matrix.CreateFrameCanvas()
-#         canvas.Clear()
-#         if len(trains):
-#             self.draw_train(0, trains[0], stop_id, canvas)
-#             if len(trains) > 1:
-#                 self.draw_train(1, trains[1], stop_id, canvas)
-#             canvas = self.matrix.SwapOnVSync(canvas)
-#             return True
-#         else:
-#             return False
-#
-#     def run(self):
-#         while True:
-#             for stop_id in self.stop_ids:
-#                 trains = get_next_trains(stop_id=stop_id)
-#                 success = self.draw_trains(trains, stop_id)
-#                 if success:
-#                     time.sleep(10)  # show display for 10 seconds before exit
+class DisplayTrains(SampleBase):
+    def __init__(self, stop_ids, *args, **kwargs):
+        super(DisplayTrains, self).__init__(*args, **kwargs)
+
+        self.stop_ids = stop_ids
+        self.font = graphics.Font()
+        # self.font.LoadFont("./fonts/7x13.bdf")
+        self.font.LoadFont("./fonts/helvR12.bdf")
+        self.circle_font = graphics.Font()
+        self.circle_font.LoadFont('./fonts/6x10.bdf')
+
+    def draw_filled_circle(self, canvas, x, y, color):
+        # # Draw circle with lines
+        # graphics.DrawLine(canvas, x - 2, y - 6, x + 2, y - 6, color)
+        # graphics.DrawLine(canvas, x - 3, y - 5, x + 3, y - 5, color)
+        # graphics.DrawLine(canvas, x - 4, y - 4, x + 4, y - 4, color)
+        # graphics.DrawLine(canvas, x - 5, y - 3, x + 5, y - 3, color)
+        # graphics.DrawLine(canvas, x - 6, y - 2, x + 6, y - 2, color)
+        # graphics.DrawLine(canvas, x - 6, y - 1, x + 6, y - 1, color)
+        # graphics.DrawLine(canvas, x - 6, y, x + 6, y, color)
+        # graphics.DrawLine(canvas, x - 6, y + 1, x + 6, y + 1, color)
+        # graphics.DrawLine(canvas, x - 6, y + 2, x + 6, y + 2, color)
+        # graphics.DrawLine(canvas, x - 5, y + 3, x + 5, y + 3, color)
+        # graphics.DrawLine(canvas, x - 4, y + 4, x + 4, y + 4, color)
+        # graphics.DrawLine(canvas, x - 3, y + 5, x + 3, y + 5, color)
+        # graphics.DrawLine(canvas, x - 2, y + 6, x + 2, y + 6, color)
+
+        # Draw circle with lines
+        graphics.DrawLine(canvas, x - 2, y - 5, x + 2, y - 5, color)
+        graphics.DrawLine(canvas, x - 3, y - 4, x + 3, y - 4, color)
+        graphics.DrawLine(canvas, x - 4, y - 3, x + 4, y - 3, color)
+        graphics.DrawLine(canvas, x - 5, y - 2, x + 5, y - 2, color)
+        graphics.DrawLine(canvas, x - 5, y - 1, x + 5, y - 1, color)
+        graphics.DrawLine(canvas, x - 5, y, x + 5, y, color)
+        graphics.DrawLine(canvas, x - 5, y + 1, x + 5, y + 1, color)
+        graphics.DrawLine(canvas, x - 5, y + 2, x + 5, y + 2, color)
+        graphics.DrawLine(canvas, x - 4, y + 3, x + 4, y + 3, color)
+        graphics.DrawLine(canvas, x - 3, y + 4, x + 3, y + 4, color)
+        graphics.DrawLine(canvas, x - 2, y + 5, x + 2, y + 5, color)
+
+    def draw_row(self,
+                 canvas,
+                 row_ind,
+                 text_colour,
+                 circle_colour,
+                 route_id,
+                 headsign_text,
+                 direction,
+                 arrival_mins):
+        # Top line
+        if row_ind == 0:
+            circle_y = 7
+            text_y = 12
+        else:
+            # Bottom line
+            circle_y = 23
+            text_y = 28
+
+        route_id_offset_width = self.circle_font.CharacterWidth(ord(route_id))
+        route_id_offset = int(route_id_offset_width / 2) - 1
+
+        minutes_text = f'{arrival_mins:2d}'
+        minutes_width = sum(self.font.CharacterWidth(ord(letter)) for letter in minutes_text)
+
+        graphics.DrawText(canvas, self.font, 0, text_y, text_colour, f'{row_ind+1}')
+        graphics.DrawText(canvas, self.font, 6, text_y, text_colour, f'.')
+        # graphics.DrawCircle(canvas, 16, circle_y, 5, circle_colour)
+        self.draw_filled_circle(canvas, 16, circle_y, circle_colour)
+        graphics.DrawText(canvas, self.circle_font, 16 - route_id_offset, text_y-1, graphics.Color(0, 0, 0), route_id)
+        # graphics.DrawText(canvas, self.font, 26, text_y, text_colour, headsign_text)
+        if direction == 'N':
+            graphics.DrawText(canvas, self.circle_font, 23, text_y - 1, text_colour, '↑')
+        else:
+            graphics.DrawText(canvas, self.circle_font, 23, text_y - 1, text_colour, '↓')
+        graphics.DrawText(canvas, self.font, 43 - minutes_width, text_y, text_colour, minutes_text)
+        graphics.DrawText(canvas, self.font, 43, text_y, text_colour, "min")
+
+    def draw_train(self, row_ind, train, stop_id, canvas):
+        arrival_mins = arrival_minutes(train, stop_id)
+        text_color = graphics.Color(0, 110, 0)
+        if arrival_mins <= 0:
+            text_color = graphics.Color(255, 66, 25)
+
+        # see: https://www.6sqft.com/did-you-know-the-mta-uses-pantone-colors-to-distinguish-train-lines/
+        if arrival_mins <= 0:
+            circle_color = graphics.Color(255, 66, 25)
+        elif train.route_id in ['B', 'D', 'F', 'M']:
+            circle_color = graphics.Color(255, 66, 25)
+        elif train.route_id in ['G', ]:
+            circle_color = graphics.Color(108, 190, 69)
+        else:
+            circle_color = graphics.Color(252, 204, 10)
+
+        if stop_id.endswith('N'):
+            direction = 'N'
+        else:
+            direction = 'S'
+
+        self.draw_row(canvas,
+                      row_ind=row_ind,
+                      text_colour=text_color,
+                      circle_colour=circle_color,
+                      route_id=train.route_id,
+                      headsign_text=train.headsign_text,
+                      direction=direction,
+                      arrival_mins=arrival_mins)
+
+    def draw_trains(self, trains, stop_id):
+        canvas = self.matrix.CreateFrameCanvas()
+        canvas.Clear()
+        if len(trains):
+            self.draw_train(0, trains[0], stop_id, canvas)
+            if len(trains) > 1:
+                self.draw_train(1, trains[1], stop_id, canvas)
+            canvas = self.matrix.SwapOnVSync(canvas)
+            return True
+        else:
+            return False
+
+    def run(self):
+        while True:
+            for stop_id in self.stop_ids:
+                trains = get_next_trains(stop_id=stop_id)
+                success = self.draw_trains(trains, stop_id)
+                if success:
+                    time.sleep(10)  # show display for 10 seconds before exit
 
 
 def arrival_time(train, stop_id):
@@ -196,8 +193,8 @@ def main():
     r_trains = get_next_trains(stop_id='R33N')
     display_trains(r_trains, stop_id='R33N')
 
-    # led_display_trains = DisplayTrains(['F23N', 'F23S', 'R33N', 'R23S'])
-    # led_display_trains.process()
+    led_display_trains = DisplayTrains(['F23N', 'F23S', 'R33N', 'R23S'])
+    led_display_trains.process()
 
     pass
 
