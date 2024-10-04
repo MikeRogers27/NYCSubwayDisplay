fd293d8a5fb78174bc50afd3352924b062d774eb
MikeR <mike.rogers@image-metrics.com>
2024-10-04 17:57:48 -0400

    jiggle the text

--- main.py
+++ main.py
@@ -103,18 +103,18 @@ class DisplayTrains(SampleBase):
         minutes_text = f'{arrival_mins:2d}'
         minutes_width = sum(self.font.CharacterWidth(ord(letter)) for letter in minutes_text)
 
-        graphics.DrawText(canvas, self.font, 2, text_y, text_colour, f'{row_ind+1}')
-        graphics.DrawText(canvas, self.font, 8, text_y, text_colour, f'.')
+        graphics.DrawText(canvas, self.font, 1, text_y, text_colour, f'{row_ind+1}')
+        graphics.DrawText(canvas, self.font, 7, text_y, text_colour, f'.')
         # graphics.DrawCircle(canvas, 16, circle_y, 5, circle_colour)
-        self.draw_filled_circle(canvas, 16, circle_y, circle_colour)
-        graphics.DrawText(canvas, self.circle_font, 16 - route_id_offset, text_y-1, graphics.Color(0, 0, 0), route_id)
+        self.draw_filled_circle(canvas, 15, circle_y, circle_colour)
+        graphics.DrawText(canvas, self.circle_font, 15 - route_id_offset, text_y-1, graphics.Color(0, 0, 0), route_id)
         # graphics.DrawText(canvas, self.font, 26, text_y, text_colour, headsign_text)
         if direction == 'N':
             graphics.DrawText(canvas, self.circle_font, 24, text_y - 1, text_colour, '↑')
         else:
             graphics.DrawText(canvas, self.circle_font, 24, text_y - 1, text_colour, '↓')
-        graphics.DrawText(canvas, self.font, 44 - minutes_width, text_y, text_colour, minutes_text)
-        graphics.DrawText(canvas, self.font, 44, text_y, text_colour, "min")
+        graphics.DrawText(canvas, self.font, 45 - minutes_width, text_y, text_colour, minutes_text)
+        graphics.DrawText(canvas, self.font, 45, text_y, text_colour, "min")
 
     def draw_train(self, row_ind, train, stop_id, canvas):
         arrival_mins = arrival_minutes(train, stop_id)
