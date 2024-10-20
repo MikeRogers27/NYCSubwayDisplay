a4b76adf419a34af94142f69757a082db02ad8d7
MikeR <mike.rogers@image-metrics.com>
2024-10-20 11:21:50 -0400

    re-enable all modes

--- main.py
+++ main.py
@@ -683,7 +683,7 @@ class RunMatrix(SampleBase):
 
     @staticmethod
     def what_should_we_display():
-        return ['sports'], 10
+        # return ['sports'], 10
 
         now = datetime.now()
         timestamp = now.time()
