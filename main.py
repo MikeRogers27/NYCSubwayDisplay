7677afe1d7d0c6cfb549ae366858c5db5ba53e7b
MikeR <mike.rogers@image-metrics.com>
2024-10-21 09:12:44 -0400

    lets see all the trains in the next 25 mins

--- main.py
+++ main.py
@@ -625,7 +625,7 @@ class RunMatrix(SampleBase):
         if uptown_only:
             stop_ids = self.uptown_stop_ids
         for stop_id in stop_ids:
-            trains = mta_get_next_trains(stop_id=stop_id, num_trains=4)
+            trains = mta_get_next_trains(stop_id=stop_id, min_num_trains=2, max_arrival_mins=25)
             success, canvas = self.draw_trains(trains, stop_id, canvas, display_time)
 
         return canvas
@@ -803,14 +803,17 @@ def mta_arrival_time(train, stop_id):
 def mta_arrival_minutes(train, stop_id):
     t = mta_arrival_time(train, stop_id)
     tdelta = t - NOW
-    arrival_mins = int(tdelta.total_seconds() / 60)
+    arrival_mins = round(tdelta.total_seconds() / 60)
     return arrival_mins
 
 
-def mta_find_next_trains(trains, num_trains, stop_id):
-    arrival_times = [mta_arrival_time(train, stop_id) for train in trains]
-    train_order = sorted(range(len(arrival_times)), key=lambda k: arrival_times[k])
-    return [trains[train_order[i]] for i in range(num_trains) if len(train_order) > i]
+def mta_find_next_trains(trains, min_num_trains, max_arrival_mins, stop_id):
+    arrival_mins = [mta_arrival_minutes(train, stop_id) for train in trains]
+    train_order = sorted(range(len(arrival_mins)), key=lambda k: arrival_mins[k])
+    next_trains = [trains[train_order[i]]
+                   for i in range(len(train_order))
+                   if i < min_num_trains or arrival_mins[train_order[i]] <= max_arrival_mins]
+    return next_trains
 
 
 def mta_get_feeds():
@@ -832,7 +835,8 @@ def mta_get_feeds():
 
 
 def mta_get_next_trains(
-        num_trains=2,
+        min_num_trains=2,
+        max_arrival_mins=25,
         stop_id='F23N'
 ):
     # time from now
@@ -844,7 +848,7 @@ def mta_get_next_trains(
         all_trains = []
         for feed in feeds:
             all_trains.extend(feed.filter_trips(headed_for_stop_id=stop_id))
-        return mta_find_next_trains(all_trains, num_trains, stop_id)
+        return mta_find_next_trains(all_trains, min_num_trains, max_arrival_mins, stop_id)
     else:
         return None
 
