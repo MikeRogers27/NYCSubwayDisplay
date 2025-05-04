4e766bcfd35712e9b6f4c6bc6bd165f8a8dd0efb
MikeR <mike.rogers@image-metrics.com>
2025-05-04 13:17:41 -0400

    enable full functions

--- main.py
+++ main.py
@@ -1324,7 +1324,7 @@ class RunMatrix(SampleBase):
 
     @staticmethod
     def what_should_we_display():
-        return ['sports'], [5]
+        # return ['sports'], [5]
 
         now = datetime.now()
         timestamp = now.time()
